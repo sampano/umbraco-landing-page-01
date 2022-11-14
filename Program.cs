341c10fe7ba8c0393c9640a4e3132eb9e877fccf
unknown <samuelpano@gmail.com>
2022-11-14 11:19:12 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+namespace Landing_Page_Project_5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => CreateHostBuilder(args)
+                .Build()
+                .Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureUmbracoDefaults()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStaticWebAssets();
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
